7c0d0a5d224efc636b9cf620816e60c7bd7f4d8b
Whitny Edwards <wne4e2@gmail.com>
2019-05-13 21:50:48 -0400

    add new view for meal_plans

--- app/controllers/meal_plans_controller.rb
+++ app/controllers/meal_plans_controller.rb
@@ -1,52 +1,51 @@
 class MealPlansController < ApplicationController
   before_action :set_meal_plan, only: [:edit, :update, :show, :destroy]
 
-def new
-  @meal_plan = Meal_plan.new
-end
-
-def create
-  @meal_plan = current_user.meal_plans.build(meal_plan_params)
-  if @meal_plan.save
-    redirect_to meal_plan_path(@meal_plan)
-  else
-    render :new
+  def index
+    @meal_plans = Meal_plan.all
   end
-end
 
-def index
-  @meal_plans = Meal_plan.all
-end
-
-def show
-
-end
-
-def edit
-
-end
-
-def update
-  if @meal_plan.update(meal_plan_params)
-    redirect_to meal_plan_path(@meal_plan)
-  else
-    render :edit
+  def show
   end
-end
 
-def destroy
-  @meal_plan.destroy
-  redirect_to meal_plans_path
-end
+  def new
+    @meal_plan = Meal_plan.new
+  end
+
+  def create
+    @meal_plan = current_user.meal_plans.build(meal_plan_params)
+    if @meal_plan.save
+      redirect_to meal_plan_path(@meal_plan)
+    else
+      render :new
+    end
+  end
+
+
+  def edit
+  end
+
+  def update
+    if @meal_plan.update(meal_plan_params)
+      redirect_to meal_plan_path(@meal_plan)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @meal_plan.destroy
+    redirect_to meal_plans_path
+  end
 
 private
 
-def meal_plan_params
-  params.require(:meal_plan).permit(:goal, :type)
-end
+  def meal_plan_params
+    params.require(:meal_plan).permit(:goal, :type)
+  end
 
-def set_meal_plan
-  @meal_plan = Meal_plan.find(params[:id])
-end
+  def set_meal_plan
+    @meal_plan = Meal_plan.find(params[:id])
+  end
 
 end
