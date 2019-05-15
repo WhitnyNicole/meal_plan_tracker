345acadb01b48a373d8d65792da90f136464989d
Whitny Edwards <wne4e2@gmail.com>
2019-05-14 20:29:13 -0400

    add bcrypt gem, update user model, update sessions home view and create user new view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,13 @@
 class SessionsController < ApplicationController
 
-def home
-end
-
 def new
 end
 
 def create
-end 
+end
+
+def home
+end
+
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :meal_plans
   has_many :meals, through: :meal_plans
 
