cc96e53feb7a14adf0a64b9fe90e60b3d8490bb3
Whitny Edwards <wne4e2@gmail.com>
2019-05-22 23:31:25 -0400

    update sessions controller create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,31 +12,9 @@ def new
   render :login
 end
 
-# def create
-#   @user = User.find_by(email: params[:user][:email])
-#   if @user && @user.authenticate(params[:user][:password])
-#     session[:user_id] = @user.id
-#     redirect_to user_path(@user)
-#   else
-#     flash[:error] = "Sorry, there was an error creating your account!"
-#     redirect_to '/login'
-#     # render :login
-#   end
-# end
-
-def fbcreate
-  @user = User.find_or_create_by(uid: auth['uid']) do |u|
-    u.name = auth['info']['name']
-    u.email = auth['info']['email']
-    u.password = SecureRandom.hex(10)
-  end
-  session[:user_id] = @user.id
-  redirect_to user_path(@user)
-end
-
 def create
   @user = User.find_by(email: params[:user][:email])
-    return head(:forbidden) unless @user.authenticate(params[:password])
+  if @user && @user.authenticate(params[:user][:password])
     session[:user_id] = @user.id
     redirect_to user_path(@user)
   else
@@ -45,6 +23,27 @@ def create
     # render :login
   end
 end
+#
+# def fbcreate
+#   @user = User.find_or_create_by(uid: auth['uid']) do |u|
+#     u.name = auth['info']['name']
+#     u.email = auth['info']['email']
+#     u.password = SecureRandom.hex(10)
+#   end
+#   session[:user_id] = @user.id
+#   redirect_to user_path(@user)
+# end
+
+# def create
+#   @user = User.find_by(email: params[:user][:email])
+#     return head(:forbidden) unless @user.authenticate(params[:password])
+#     session[:user_id] = @user.id
+#     redirect_to user_path(@user)
+#   else
+#     flash[:error] = "Sorry, there was an error creating your account!"
+#     redirect_to '/login'
+#   end
+# end
 
 
 
@@ -57,3 +56,4 @@ private
   def auth
     request.env['omniauth.auth']
   end
+end 
