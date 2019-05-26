e6a9454d79865768f2f3be259b8434ebfd510d22
Whitny Edwards <wne4e2@gmail.com>
2019-05-25 21:10:54 -0400

    update sessions controller to ensure password confirmation is correct before logging in a user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,7 @@ end
 
 def create
   @user = User.find_by(email: params[:user][:email])
-  if @user && @user.authenticate(params[:user][:password])
+  if @user && @user.authenticate(params[:user][:password]) && @user.authenticate(params[:user][:password_confirmation])
     session[:user_id] = @user.id
     redirect_to user_path(@user)
   else
