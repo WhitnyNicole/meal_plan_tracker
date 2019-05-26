deb3b6a07812bb77cd44b2b42bc15efec818b755
Whitny Edwards <wne4e2@gmail.com>
2019-05-26 17:55:27 -0400

    add validation of meal plan id to meal model

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -3,9 +3,10 @@ class Meal < ApplicationRecord
   has_many :meal_schedules
 
   validates :food, :day, presence: true
+  validates :meal_plan_id, presence: true
 
   # accepts_nested_attributes_for :meal_plan, reject_if: :all_blank
-  # 
+  #
   # def meal_plan_attributes=(attributes)
   #   mealplan = MealPlan.find_or_create_by(attributes)
   #   self.mealplan = mealplan if mealplan.valid?
