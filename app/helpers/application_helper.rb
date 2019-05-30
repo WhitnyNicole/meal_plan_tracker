4b08b85f721bad5b9644831117c5b111417a633f
Whitny Edwards <wne4e2@gmail.com>
2019-05-29 23:18:52 -0400

    update navigation to show certain options only to logged in users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,10 @@
 module ApplicationHelper
   def current_user
-      @current_user ||= User.find_by_id(session[:user_id])
+      @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
     end
 
-    def logged_in?
-      !!session[:user_id]
-    end
+  def logged_in?
+    !!session[:user_id]
+      # !!@current_user
+  end
 end
