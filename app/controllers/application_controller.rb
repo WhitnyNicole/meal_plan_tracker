df04ab8fbd480ebca01dbf6cb295764e26c3b6bd
Whitny Edwards <wne4e2@gmail.com>
2019-06-01 16:06:40 -0400

    update redirect_if_not_logged_in method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include ApplicationHelper
-  
+
 private
 
-    def redirect_if_not_logged_in
+  def redirect_if_not_logged_in
     if !logged_in?
-      flash[:errors] = "You must be logged in to view the page you tried to view."
+      flash[:danger] = "You must be logged in to view the page you tried to view."
       redirect_to '/'
     end
   end
