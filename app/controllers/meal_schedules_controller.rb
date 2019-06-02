ebe731fb777f26703ec56e7cb5896c8852213587
Whitny Edwards <wne4e2@gmail.com>
2019-06-01 20:24:20 -0400

    update before actions

--- app/controllers/meal_schedules_controller.rb
+++ app/controllers/meal_schedules_controller.rb
@@ -1,6 +1,6 @@
 class MealSchedulesController < ApplicationController
 before_action :set_meal_schedules, only: [:show, :edit]
-before_action :redirect_if_not_logged_in, only: [:new, :create, :edit, :update]
+before_action :redirect_if_not_logged_in, only: [:new, :create, :edit, :update, :show]
 before_action :require_same_user, only: [:edit, :update, :delete]
 
   def index
@@ -12,9 +12,6 @@ before_action :require_same_user, only: [:edit, :update, :delete]
     end
   end
 
-  def show
-  end
-
   def new
     if params[:meal_id] && meal = Meal.find_by_id(params[:meal_id])
       @meal_schedule = meal.meal_schedules.build
@@ -23,21 +20,25 @@ before_action :require_same_user, only: [:edit, :update, :delete]
     end
   end
 
-  def create
-    # @meal_schedule = MealSchedule.new(meal_schedule_params)
-    @meal_schedule = current_meal.meal_schedules.build(meal_schedule_params)
-    if @meal_schedule.save
-      flash[:success] = "Your meal schedule was created!"
-      redirect_to meal_path(@meal_schedule.meal)
-    else
-      render :new
+    def create
+      # @meal_schedule = MealSchedule.new(meal_schedule_params)
+      @meal_schedule = current_meal.meal_schedules.build(meal_schedule_params)
+      if @meal_schedule.save
+        flash[:success] = "Your meal schedule was created!"
+        redirect_to meal_schedule_path(@meal_schedule)
+      else
+        render :new
+      end
     end
+
+  def show
   end
 
   def edit
   end
 
   def update
+    set_meal_schedules
     if @meal_schedule.update(meal_schedule_params)
       flash[:success] = "Your meal schedule was updated!"
       redirect_to meal_schedule_path(@meal_schedule)
@@ -47,6 +48,7 @@ before_action :require_same_user, only: [:edit, :update, :delete]
   end
 
   def destroy
+    set_meal_schedules
     @meal_schedule.destroy
     flash[:success] = "Your meal schedule was deleted!"
     redirect_to meal_schedules_path
