341518e644bcf707038a3b0d4e1f2450808bb955
Whitny Edwards <wne4e2@gmail.com>
2019-06-03 19:05:53 -0400

    add association between user and meal schedule on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ApplicationRecord
   has_secure_password
   has_many :meal_plans
   has_many :meals, through: :meal_plans
-
+  has_many :meal_schedules, through: :meals
 
   validates :name, :email, presence: true
   validates :email, uniqueness: true
+
 end
