262756f83bd94feee2c9ab9ed85bdcf7cdc4d0a9
Whitny Edwards <wne4e2@gmail.com>
2019-06-05 19:34:26 -0400

    remove meal_plan_id from meals table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190603234655) do
+ActiveRecord::Schema.define(version: 20190605233302) do
 
   create_table "meal_plans", force: :cascade do |t|
     t.string   "goal"
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(version: 20190603234655) do
   create_table "meals", force: :cascade do |t|
     t.string   "protein"
     t.datetime "day"
-    t.integer  "meal_plan_id"
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
     t.boolean  "favorite",        default: false
@@ -43,7 +42,6 @@ ActiveRecord::Schema.define(version: 20190603234655) do
     t.string   "beverage"
     t.string   "vegetable"
     t.string   "side"
-    t.index ["meal_plan_id"], name: "index_meals_on_meal_plan_id"
   end
 
   create_table "users", force: :cascade do |t|
