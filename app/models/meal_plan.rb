c46d0f41dbe6929f5406e4ca8f4dc0946f339b69
Whitny Edwards <wne4e2@gmail.com>
2019-06-06 19:32:55 -0400

    refactor accepts nested attributes for meal schedules

--- app/models/meal_plan.rb
+++ app/models/meal_plan.rb
@@ -1,10 +1,12 @@
 class MealPlan < ApplicationRecord
   belongs_to :user
-  has_many :meals, dependent: :destroy
-  has_many :meal_schedules, through: :meals, dependent: :destroy
+  has_many :meals_schedules, dependent: :destroy
+  has_many :meals, through: :meal_schedules, dependent: :destroy
+  accepts_nested_attributes_for :meal_schedules, reject_if: :all_blank
+
 
   validates :goal, :description, presence: true
   default_scope -> { order(updated_at: :desc)}
 
-  accepts_nested_attributes_for :meal_schedules, reject_if: :all_blank
+
 end
