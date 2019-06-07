083ac4bbe5a257b3e69815e7afda227c0413627c
Whitny Edwards <wne4e2@gmail.com>
2019-06-07 17:28:58 -0400

    remove require same user method and associated before action

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,7 +1,7 @@
 class MealsController < ApplicationController
 before_action :set_meal, only: [:show, :edit]
 before_action :redirect_if_not_logged_in, only: [:new, :create, :edit, :update, :show]
-before_action :require_same_user, only: [:edit, :update, :delete]
+# before_action :require_same_user, only: [:edit, :update, :delete]
 
 def index
   if params[:meal_plan_id] && mealplan = MealPlan.find_by_id(params[:meal_plan_id])
@@ -72,11 +72,11 @@ private
     # end
   end
 
-  def require_same_user
-    set_meal
-    if current_user.id != @meal.meal_plan.user_id
-      flash[:danger] = "You can only edit or delete your own meal"
-      redirect_to meals_path
-    end
-  end
+  # def require_same_user
+  #   set_meal
+  #   if current_user.id != @meal.meal_plan.user_id
+  #     flash[:danger] = "You can only edit or delete your own meal"
+  #     redirect_to meals_path
+  #   end
+  # end
 end
