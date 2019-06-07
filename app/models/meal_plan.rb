92a3219734f451d85d56675866e2b0119ccdc2da
Whitny Edwards <wne4e2@gmail.com>
2019-06-07 14:17:21 -0400

    remove nested attributes

--- app/models/meal_plan.rb
+++ app/models/meal_plan.rb
@@ -2,11 +2,10 @@ class MealPlan < ApplicationRecord
   belongs_to :user
   has_many :meal_schedules, dependent: :destroy
   has_many :meals, through: :meal_schedules, dependent: :destroy
-  accepts_nested_attributes_for :meal_schedules, reject_if: :all_blank
+  # accepts_nested_attributes_for :meal_schedules, reject_if: :all_blank
 
 
   validates :goal, :description, presence: true
   default_scope -> { order(updated_at: :desc)}
-
 
 end
