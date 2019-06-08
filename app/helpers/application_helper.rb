46955e9d4fbcc3f33dcf5bfded6390a52255efff
Whitny Edwards <wne4e2@gmail.com>
2019-06-08 17:09:35 -0400

    update line spacing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   def current_user
-      @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
-    end
+    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+  end
 
   def logged_in?
     !!session[:user_id]
-      # !!@current_user
+  # !!@current_user
   end
 
   def current_meal
