f1b94f63244216983634b90f16262e2c3e3cef20
Whitny Edwards <wne4e2@gmail.com>
2019-06-08 17:08:33 -0400

    update line spacing

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -3,60 +3,60 @@ before_action :set_meal, only: [:show]
 # before_action :redirect_if_not_logged_in, only: [:new, :create, :edit, :update, :show]
 # before_action :require_same_user, only: [:edit, :update, :delete]
 
-def index
-  if params[:meal_plan_id] && mealplan = MealPlan.find_by_id(params[:meal_plan_id])
-    @meals = mealplan.meals.paginate(page: params[:page], per_page: 2)
+  def index
+    if params[:meal_plan_id] && mealplan = MealPlan.find_by_id(params[:meal_plan_id])
+      @meals = mealplan.meals.paginate(page: params[:page], per_page: 2)
     # @meals = Meal.paginate(page: params[:page], per_page: 5)
-  else
-    @meals = current_user.meals.paginate(page: params[:page], per_page: 2)
+    else
+      @meals = current_user.meals.paginate(page: params[:page], per_page: 2)
+    end
   end
-end
 
-# def new
-#   redirect_if_not_logged_in
-#   if current_user && params[:meal_plan_id] && @mealplan = MealPlan.find_by_id(params[:meal_plan_id])
-#     @mealplan.meals.build
-#   else
-#     @meal = Meal.new
-#     # @meal.build_meal_plan
-#   end
-# end
+  # def new
+  #  redirect_if_not_logged_in
+  #  if current_user && params[:meal_plan_id] && @mealplan = MealPlan.find_by_id(params[:meal_plan_id])
+  #    @mealplan.meals.build
+  #  else
+  #    @meal = Meal.new
+  #  # @meal.build_meal_plan
+  #  end
+  # end
 
 # def create
-#   @meal = Meal.create(meal_params)
-#   if @meal.save
-#     flash[:success] = "Your meal was created!"
-#     # redirect_to meal_plan_path(@meal.meal_plan)
-#     redirect_to meal_path(@meal)
-#   else
-#     # @meal.build_meal_plan unless @meal.meal_plan
-#     render :new
-#   end
+#  @meal = Meal.create(meal_params)
+#  if @meal.save
+#    flash[:success] = "Your meal was created!"
+#  # redirect_to meal_plan_path(@meal.meal_plan)
+#    redirect_to meal_path(@meal)
+#  else
+#  # @meal.build_meal_plan unless @meal.meal_plan
+#    render :new
+#  end
 # end
 
-def show
-end
+  def show
+  end
 
 # def edit
 # end
 
 # def update
-#   set_meal
-#   redirect_if_not_logged_in
-#   if @meal.update(meal_params)
-#     flash[:success] = "Your meal was updated!"
-#     redirect_to meal_path(@meal)
-#   else
-#     render :edit
-#   end
+#  set_meal
+#  redirect_if_not_logged_in
+#  if @meal.update(meal_params)
+#    flash[:success] = "Your meal was updated!"
+#    redirect_to meal_path(@meal)
+#  else
+#    render :edit
+#  end
 # end
 
 # def destroy
-#   set_meal
-#   redirect_if_not_logged_in
-#   @meal.destroy
-#   flash[:success] = "Your meal was deleted!"
-#   redirect_to meals_path
+#  set_meal
+#  redirect_if_not_logged_in
+#  @meal.destroy
+#  flash[:success] = "Your meal was deleted!"
+#  redirect_to meals_path
 # end
 
 
@@ -73,10 +73,10 @@ private
   end
 
   # def require_same_user
-  #   set_meal
-  #   if current_user.id != @meal.meal_plan.user_id
-  #     flash[:danger] = "You can only edit or delete your own meal"
-  #     redirect_to meals_path
-  #   end
-  # end
+  #  set_meal
+  #  if current_user.id != @meal.meal_plan.user_id
+  #    flash[:danger] = "You can only edit or delete your own meal"
+  #    redirect_to meals_path
+  #  end
+  #end
 end
