1c8511875c046570d2d429d4eb869947514974ff
Whitny Edwards <wne4e2@gmail.com>
2019-06-08 17:08:49 -0400

    update line spacing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,63 +1,62 @@
 class SessionsController < ApplicationController
+  require 'securerandom'
 
-require 'securerandom'
-
-def destroy
-  session.clear
-  flash[:success] = "You have logged out"
-  redirect_to '/'
-end
-
-def new
-  @user = User.new
-  render :login
-end
-
-def create
-  @user = User.find_by(email: params[:user][:email])
-  if @user && @user.authenticate(params[:user][:password]) && @user.authenticate(params[:user][:password_confirmation])
-    session[:user_id] = @user.id
-    flash[:success] = "Welcome back #{@user.name}!"
-    redirect_to user_path(@user)
-  else
-    flash[:error] = "Sorry, there was an error with your account!"
-    redirect_to '/login'
-    # render :login
+  def destroy
+    session.clear
+    flash[:success] = "You have logged out"
+    redirect_to '/'
   end
-end
 
-def fbcreate
-  @user = User.find_or_create_by(uid: auth['uid']) do |u|
-    u.name = auth['info']['name']
-    u.email = auth['info']['email']
-    u.password = SecureRandom.hex(10)
+  def new
+    @user = User.new
+    render :login
   end
-  session[:user_id] = @user.id
-  flash[:success] = "Welcome back #{@user.name}!"
-  redirect_to user_path(@user)
-end
+
+  def create
+    @user = User.find_by(email: params[:user][:email])
+    if @user && @user.authenticate(params[:user][:password]) && @user.authenticate(params[:user][:password_confirmation])
+      session[:user_id] = @user.id
+      flash[:success] = "Welcome back #{@user.name}!"
+      redirect_to user_path(@user)
+    else
+      flash[:error] = "Sorry, there was an error with your account!"
+      redirect_to '/login'
+      # render :login
+    end
+  end
+
+  def fbcreate
+    @user = User.find_or_create_by(uid: auth['uid']) do |u|
+      u.name = auth['info']['name']
+      u.email = auth['info']['email']
+      u.password = SecureRandom.hex(10)
+    end
+      session[:user_id] = @user.id
+      flash[:success] = "Welcome back #{@user.name}!"
+      redirect_to user_path(@user)
+  end
 
 # def create
 #   @user = User.find_by(email: params[:user][:email])
-#     return head(:forbidden) unless @user.authenticate(params[:password])
-#     session[:user_id] = @user.id
-#     redirect_to user_path(@user)
+#    return head(:forbidden) unless @user.authenticate(params[:password])
+#    session[:user_id] = @user.id
+#    redirect_to user_path(@user)
 #   else
-#     flash[:error] = "Sorry, there was an error creating your account!"
-#     redirect_to '/login'
+#    flash[:error] = "Sorry, there was an error creating your account!"
+#    redirect_to '/login'
 #   end
 # end
 
 
 
-def home
-  redirect_to meal_plans_path if logged_in?
-end
-
-
-
-private
-  def auth
-    request.env['omniauth.auth']
+  def home
+    redirect_to meal_plans_path if logged_in?
   end
-end
+
+
+
+  private
+    def auth
+      request.env['omniauth.auth']
+    end
+  end
