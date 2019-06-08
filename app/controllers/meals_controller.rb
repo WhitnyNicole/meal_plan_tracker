69fcdb211326e070059243d0c2fd99214f6a355e
Whitny Edwards <wne4e2@gmail.com>
2019-06-07 20:41:25 -0400

    update meals controller show only index and show methods remain

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,6 +1,6 @@
 class MealsController < ApplicationController
-before_action :set_meal, only: [:show, :edit]
-before_action :redirect_if_not_logged_in, only: [:new, :create, :edit, :update, :show]
+before_action :set_meal, only: [:show]
+# before_action :redirect_if_not_logged_in, only: [:new, :create, :edit, :update, :show]
 # before_action :require_same_user, only: [:edit, :update, :delete]
 
 def index
@@ -12,52 +12,52 @@ def index
   end
 end
 
-def new
-  redirect_if_not_logged_in
-  if current_user && params[:meal_plan_id] && @mealplan = MealPlan.find_by_id(params[:meal_plan_id])
-    @mealplan.meals.build
-  else
-    @meal = Meal.new
-    # @meal.build_meal_plan
-  end
-end
+# def new
+#   redirect_if_not_logged_in
+#   if current_user && params[:meal_plan_id] && @mealplan = MealPlan.find_by_id(params[:meal_plan_id])
+#     @mealplan.meals.build
+#   else
+#     @meal = Meal.new
+#     # @meal.build_meal_plan
+#   end
+# end
 
-def create
-  @meal = Meal.create(meal_params)
-  if @meal.save
-    flash[:success] = "Your meal was created!"
-    # redirect_to meal_plan_path(@meal.meal_plan)
-    redirect_to meal_path(@meal)
-  else
-    # @meal.build_meal_plan unless @meal.meal_plan
-    render :new
-  end
-end
+# def create
+#   @meal = Meal.create(meal_params)
+#   if @meal.save
+#     flash[:success] = "Your meal was created!"
+#     # redirect_to meal_plan_path(@meal.meal_plan)
+#     redirect_to meal_path(@meal)
+#   else
+#     # @meal.build_meal_plan unless @meal.meal_plan
+#     render :new
+#   end
+# end
 
 def show
 end
 
-def edit
-end
+# def edit
+# end
 
-def update
-  set_meal
-  redirect_if_not_logged_in
-  if @meal.update(meal_params)
-    flash[:success] = "Your meal was updated!"
-    redirect_to meal_path(@meal)
-  else
-    render :edit
-  end
-end
+# def update
+#   set_meal
+#   redirect_if_not_logged_in
+#   if @meal.update(meal_params)
+#     flash[:success] = "Your meal was updated!"
+#     redirect_to meal_path(@meal)
+#   else
+#     render :edit
+#   end
+# end
 
-def destroy
-  set_meal
-  redirect_if_not_logged_in
-  @meal.destroy
-  flash[:success] = "Your meal was deleted!"
-  redirect_to meals_path
-end
+# def destroy
+#   set_meal
+#   redirect_if_not_logged_in
+#   @meal.destroy
+#   flash[:success] = "Your meal was deleted!"
+#   redirect_to meals_path
+# end
 
 
 private
