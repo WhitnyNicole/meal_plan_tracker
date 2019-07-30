3aa99aef7e4464bdab6c0980ce888eb697b1c084
Whitny Edwards <wne4e2@gmail.com>
2019-07-30 14:11:45 -0400

    add event listeners

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,5 +1,53 @@
-$(document).on('turbolinks:load', function() {
-  $('.my_meal').on('click', function(event) {
-    console.log("clicked!");
-  });
+
+// $(document).on('turbolinks:load', function() {
+//   addMealEventListener();
+// })
+$(document).ready(function() {
+  addMealEventListener();
 })
+
+function addMealEventListener() {
+$('#myMeal').on('click', function(event) {
+  const mealId = $(this).data("mealId");
+  event.preventDefault();
+  fetch(`/meals/${mealId}`)
+    .then(function(response) {
+      debugger
+      return response.json();
+    })
+    .then(function(data) {
+      debugger
+    })
+});
+}
+
+
+
+
+
+
+//
+// $(document).on('turbolinks:load', function() {
+//   $('.my_meal').on('click', function(event) {
+//     const mealdata = $(this).data(".my_meal");
+//
+//     fetch(`/meals/${id}`)
+//     .then(function(response) {
+//       return response.json();
+//     })
+//     .then(function(data){
+//       $(".my_meal").html(`
+//         <h1> Protein: $()
+//         `)
+//     })
+//   });
+// })
+
+
+// $(document).on('turbolinks:load', function() {
+//   alert("hello");
+// });
+//
+// $(document).ready(function() {
+//   alert("hello again");
+// });
