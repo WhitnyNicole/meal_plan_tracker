639b4c227d6f478d48152b5a83eff01924980e75
Whitny Edwards <wne4e2@gmail.com>
2019-08-02 19:56:13 -0400

    update meal plan serializer to add in associations and additional attributes

--- app/serializers/meal_plan_serializer.rb
+++ app/serializers/meal_plan_serializer.rb
@@ -1,3 +1,5 @@
 class MealPlanSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :goal, :description
+  has_many :meal_schedules
+  has_many :meals, through: :meal_schedules
 end
