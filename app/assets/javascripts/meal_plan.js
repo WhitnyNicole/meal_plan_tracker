4b726d7492661eb75f466dddae2ecd001cd27926
Whitny Edwards <wne4e2@gmail.com>
2019-08-02 19:39:09 -0400

    add js file for meal plan

---
+++ app/assets/javascripts/meal_plan.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+  addMealPlanEventListener();
+})
+
+function addMealPlanEventListener() {
+$('.myMealPlan').on('click', function(event) {
+  const mealPlanId = $(this).data("mealPlanId");
+  event.preventDefault();
+  fetch(`/meal_plans/${mealId}.json`)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+    let mymealplan = new MealPlan(data)
+    debugger
+    let myMealPlanHTML = mymealplan.mealPlanHTML()
+    // document.getElementsByClassName('myMeal').innerHTML += myMealHTML
+    // $(`ul#meal-33`).html(data)
+    document.getElementById(`meal-33`).innerHTML = myMealHTML
+    })
+});
+}
+
+
+class MealPlan {
+  constructor(data) {
+    this.id = data.id
+    this.goal = data.goal
+    this.description = data.description
+  }
+
+
+
+
+  MealPlan.prototype.mealHTML = function () {
+    return (`
+      <ul>
+        <li>Goal: ${this.goal}</li>
+        <li>Description: ${this.description}</li>
+      </ul>
+      `)
+  }
