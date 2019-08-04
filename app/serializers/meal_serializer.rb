9704f60e43ca2b557df96a1c36f25991f0342993
Whitny Edwards <wne4e2@gmail.com>
2019-08-04 18:17:10 -0400

    remove meal plan association from meal serializer

--- app/serializers/meal_serializer.rb
+++ app/serializers/meal_serializer.rb
@@ -1,5 +1,5 @@
 class MealSerializer < ActiveModel::Serializer
   attributes :id, :protein, :vegetable, :side, :beverage, :pretty_created_date
   has_many :meal_schedules
-  has_many :meal_plan through: :meal_schedules 
+  # has_many :meal_plans through: :meal_schedules 
 end
