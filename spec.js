d702a0fa9fb50c9423141279c4fd43f27f71cd91
Whitny Edwards <wne4e2@gmail.com>
2019-08-10 19:04:41 -0400

    add spec.js file for project requirements

---
+++ spec.js
@@ -0,0 +1,25 @@
+Rails App with JavaScript Frontend Spec
+
+Project Specs:
+[X]Must have a Rails Backend and new requirements implemented through JavaScript.
+[X]Makes use of ES6 features as much as possible(e.g Arrow functions, Let & Const, Constructor Functions)
+[X]Must translate the JSON responses into Javascript Model Objects using either ES6 class or constructor syntax.
+[X]Must render at least one index page (index resource - 'list of things') via JavaScript and an Active Model Serialization JSON Backend.
+[X]Must render at least one show page (show resource - 'one specific thing') via JavaScript and an Active Model Serialization JSON Backend.
+[X]Your Rails application must reveal at least one `has-many` relationship through JSON that is then rendered to the page.
+[X]Must use your Rails application to render a form for creating a resource that is submitted dynamically through JavaScript.
+[X]At least one of the JS Model Objects must have a method on the prototype.
+
+Project Repo Specs:
+
+Read Me file contains:
+[X]Application Description
+[X]Installation guide (e.g. fork and clone repo, migrate db, bundle install, etc)
+[X]Contributors guide (e.g. file an issue, file an issue with a pull request, etc)
+[X]Licensing statement at the bottom (e.g. This project has been licensed under the MIT open source license.)
+
+Repo General
+[X]You have a large number of small Git commits
+[X]Your commit messages are meaningful
+[]You made the changes in a commit that relate to the commit message
+[X]You don't include changes in a commit that aren't related to the commit message
