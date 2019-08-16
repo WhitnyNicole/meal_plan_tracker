1c736b50f6594d82ef7b795697693ec5b57d2bfc
Whitny Edwards <wne4e2@gmail.com>
2019-08-15 20:40:01 -0400

    update to add custom methods

--- app/controllers/meal_plans_controller.rb
+++ app/controllers/meal_plans_controller.rb
@@ -1,14 +1,25 @@
 class MealPlansController < ApplicationController
-  before_action :set_meal_plan, only: [:edit, :update, :show, :delete]
+  before_action :set_meal_plan, only: [:edit, :update, :show, :delete, :like]
   before_action :redirect_if_not_logged_in, only: [:new, :create, :edit, :update]
   before_action :require_same_user, only: [:edit, :update, :delete]
-
+  before_action :require_user_like, only: [:like]
   def index
     # @meal_plans = current_user.meal_plans
     @meal_plans = current_user.meal_plans.paginate(page: params[:page], per_page: 2)
     respond_to do |format|
       format.html { render :index }
       format.json { render json: @meal_plans, status: 200 }
+    end
+  end
+
+  def like
+    like = Like.create(like: params[:like], user: current_user, meal_plan: @meal_plan)
+    if like.valid?
+      flash[:success] = "Your selection was succesful"
+      redirect_to :back
+    else
+      flash[:danger] = "You can only like/dislike a meal plan once"
+      redirect_to :back
     end
   end
 
@@ -83,6 +94,13 @@ private
     if current_user.id != @meal_plan.user_id
       flash[:danger] = "You can only edit or delete your own meal plan"
       redirect_to meal_plans_path
+    end
+  end
+
+  def require_user_like
+    if !logged_in?
+    flash[:danger] = "You must be logged in to perform that action"
+    redirect_to :back
     end
   end
 end
