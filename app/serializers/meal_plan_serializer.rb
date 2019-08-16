0dc455f8a158835314ab8c4176322d3e7c128d49
Whitny Edwards <wne4e2@gmail.com>
2019-08-15 21:49:28 -0400

    add likes attribute

--- app/serializers/meal_plan_serializer.rb
+++ app/serializers/meal_plan_serializer.rb
@@ -1,5 +1,5 @@
 class MealPlanSerializer < ActiveModel::Serializer
-  attributes :id, :goal, :description, :rating
+  attributes :id, :goal, :description, :likes
   has_many :meal_schedules
   has_many :meals, through: :meal_schedules
 end
